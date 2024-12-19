3cb65a6ea91ac5bbce124f0d7a677c92cc8567fc
Ульяна Сухова <ulanasuhova@MacBook-Air-de-Ulana.local>
2024-12-19 20:13:43 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,42 @@
+import pygame as pg
+import pytmx
+
+pg.init()
+
+SCREEN_WIDTH=800
+SCREEN_HEIGHT=600
+
+class Game():
+    def __init__(self):
+        self.screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        pg.display.set_caption('платформер')
+
+        self.tmx_map = pytmx.load_pygame('levels/level1.tmx')
+
+        self.run()
+    def run(self):
+        while True:
+            self.event()
+            self.update()
+            self.draw()
+    def event(self):
+        for event in pg.event.get():
+            if event.type==pg.QUIT:
+                pg.quit()
+                exit()
+    def update(self):
+        ...
+    def draw(self):
+        self.screen.fill('light blue')
+
+        for layer in self.tmx_map:
+            for x,y,gid in layer:
+                tile = self.tmx_map.get_tile_image_by_gid(gid)
+
+                if tile:
+                    self.screen.blit(tile, (x*self.tmx_map.tilewidth, y*self.tmx_map.tileheight))
+
+        pg.display.flip()
+
+if __name__=="__main__":
+    game1=Game() #создали объект класса Game()
