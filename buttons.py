1fe0dbea784c862854fed19f4efd2b71b597c055
sukhovaul <sukhovaul@yandex.ru>
2025-01-14 20:48:58 +0300

    a

--- buttons.py
+++ buttons.py
@@ -1,9 +1,13 @@
 import pygame as pg
 
-def draw_button(screen, rect, text, rect_color, btn_width, image_path = None):
+def draw_button(screen, rect, text, rect_color,color, btn_width, image_path = None):
     pg.draw.rect(screen, rect_color, rect, btn_width)
 
     if image_path:
         button_image = pg.image.load(image_path)
         image_rect = button_image.get_rect(center = (rect[0]+ rect.width//2, rect[1] + rect.height//2))
         screen.blit(button_image, image_rect)
+
+    text_surface = pg.font.render(text, True, color)
+    text_rect = text_surface.get_rect(center=(rect[0]+rect.width//2, rect[1]+rect.height//2))
+    screen.blit(text_surface, text_rect)
